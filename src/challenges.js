9921089606c8f2930f72b8f75223c1b330d75273
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-18 15:12:48 -0300

    Desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,9 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aquiii
+function compareTrue(param1, param2) {
+  if (param1 && param2) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
