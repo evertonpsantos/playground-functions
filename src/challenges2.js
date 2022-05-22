21bc18fd03ca394cdb18e93c3367396938f54d5f
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-22 20:13:58 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,26 +1,31 @@
 // Desafio 11
 function generatePhoneNumber(phoneNumber) {
-  if (phoneNumber.length < 11) {
+  let repeatCounter = 0;
+  if (phoneNumber.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
   for (let i = 0; i < phoneNumber.length; i += 1) {
-    if (phoneNumber[i] < 0) {
+    if (phoneNumber[i] < 0 || phoneNumber[i] > 9) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
-    if (phoneNumber[i] > 9) {
+    for (let index = 0; index < phoneNumber.length; index += 1) {
+      if (phoneNumber[index] === phoneNumber[i]) {
+        repeatCounter += 1;
+      }
+    }
+    if (repeatCounter >= 3) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
+    repeatCounter = 0;
   }
 
-  let areaCode = phoneNumber.slice(0, 2);
-  let firstPart = phoneNumber.slice(2, 7);
-  let secondPart = phoneNumber.slice(7);
-  let auxArray = [areaCode, firstPart, secondPart];
-  let auxArray2 = auxArray.join('');
-  console.log(auxArray2);
+  let joinAux = phoneNumber.join('');
+  let areaCode = joinAux.slice(0, 2);
+  let firstPart = joinAux.slice(2, 7);
+  let secondPart = joinAux.slice(7);
+  let auxArray = '(' + areaCode + ')' + ' ' + firstPart + '-' + secondPart;
+  return auxArray;
 }
-
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
