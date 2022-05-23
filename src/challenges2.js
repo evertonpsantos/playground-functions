94b5c1f6884fe0e67bf35d6a10d6dc9adee8b719
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-22 22:36:37 -0300

    Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 // Desafio 11
 function generatePhoneNumber(phoneNumber) {
   let repeatCounter = 0;
@@ -28,8 +29,17 @@ function generatePhoneNumber(phoneNumber) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
+    return true;
+  }
+  if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) {
+    return true;
+  }
+  if (lineC < lineB + lineA && lineC > Math.abs(lineB - lineA)) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 13
